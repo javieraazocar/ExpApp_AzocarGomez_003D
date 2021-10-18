fd641c3c8e106dd44ac5213c75c5067d44444c71
Mario Gómez <ma.gomez@alumnos.duoc.cl>
2021-10-18 19:31:53 -0300

    Creado formulario de inicio de sesión

--- src/app/pages/iniciosesion/iniciosesion.page.ts
+++ src/app/pages/iniciosesion/iniciosesion.page.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IniciosesionPage implements OnInit {
 
+  usuario = {
+    email: '',
+    password: ''
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log('submit');
+    console.log(this.usuario);
+  }
 }
